6677df7530574160231fc097e26976f6f3bb2a02
Matthew Lewis <matt@mplewis.com>
2015-04-02 19:07:42 -0500

    Update 2015-04-03T00:07:39.131Z

--- src/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  JSON_POJO_ENDPOINT: 'http://json-pojo-server-99a32bbe-1.mplewis.cont.tutum.io:9980/'
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var Layout = require('./components/layout')
-
-var CompileStore = require('./stores/compile-store')
-
-React.render(<Layout />, document.getElementById('app'))
